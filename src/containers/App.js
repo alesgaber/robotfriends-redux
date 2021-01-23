b0f9b790f3a2bcc56eb8a8a4952c656df7db62fd
alesgaber <ales.gaber@gmail.com>
2021-01-23 18:22:30 +0100

    implementing redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,55 @@
-import React, { Component } from 'react';
-import CardList from '../components/CardList';
-import SearchBox from '../components/SearchBox';
-import './App.css';
+import React, { Component } from 'react'
+import CardList from '../components/CardList'
+import SearchBox from '../components/SearchBox'
+import './App.css'
 import Scroll from '../components/Scroll'
 import ErrorBoundry from '../components/ErrorBoundry'
+import { connect } from 'react-redux'
+import { setSearchField, requestRobots } from '../actions'
+
+// what piece of state i must to listen
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchRobots.searchField,
+    robots:state.requestRobots.robots,
+    isPending: state.requestRobots.isPenfding,
+    error: state.requestRobots.error,
+  }
+}
+// return object that contains our actions (I'am interested in this actions)
+const mapDispatchToProps = dispatch => {
+  return {    
+    onSearchChange: event => dispatch(setSearchField(event.target.value)),
+    onRequestRobots: ()=> dispatch(requestRobots())
+  }
+}
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      searchfield: '',
-      robots: [],
-    };
+ 
+  componentDidMount () {
+    this.props.onRequestRobots();
   }
 
-  onSearchChanged = event => {
-    this.setState({ searchfield: event.target.value });
-  };
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }));
-  }
-
-  render() {
-    const {robots, searchfield} = this.state;
+  render () {
+    
+    const { searchField, onSearchChange, robots, isPending } = this.props;
+console.log(this.props);
     const filteredRobots = robots.filter(robot =>
-      robot.name.toLowerCase().includes(searchfield.toLowerCase())
-    );
-    return !robots.length ? 
-    <h1>Loading...</h1> :    
-      (
-        <div className="tc">
-          <h1 className="f2">RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChanged} />
-          <Scroll>
-            <ErrorBoundry>
-              <CardList robots={filteredRobots} />
-            </ErrorBoundry>            
-          </Scroll>
-          
-        </div>
-      );
-    }
+      robot.name.toLowerCase().includes(searchField.toLowerCase())
+    )
+    return isPending
+      ? <h1>Loading...</h1>
+      : <div className='tc'>
+        <h1 className='f2'>RoboFriends</h1>
+        <SearchBox searchChange={onSearchChange} />
+        <Scroll>
+          <ErrorBoundry>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundry>
+        </Scroll>
+      </div>
   }
+}
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+// connect is higher-order function..function thats return another function and run App
